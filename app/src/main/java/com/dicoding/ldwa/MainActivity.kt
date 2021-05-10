8d268605a5e3cca1e046eb7e07159f8c051ff8fa
Your Name <irshalfir@gmail.com>
2021-05-10 13:20:21 +0700

    add view model and show data to activity

--- app/src/main/java/com/dicoding/ldwa/MainActivity.kt
+++ app/src/main/java/com/dicoding/ldwa/MainActivity.kt
@@ -2,10 +2,46 @@ package com.dicoding.ldwa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import androidx.activity.viewModels
+import com.bumptech.glide.Glide
+import com.dicoding.ldwa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val mainViewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        supportActionBar?.hide()
+
+        mainViewModel.restaurant.observe(this, { restaurant ->
+            activityMainBinding.tvTitle.text = restaurant.name
+            activityMainBinding.tvDescription.text = restaurant.description
+            Glide.with(this)
+                .load("https://restaurant-api.dicoding.dev/images/large/${restaurant.pictureId}")
+                .into(activityMainBinding.ivPicture)
+        })
+
+        mainViewModel.listReview.observe(this, { customerReviews ->
+            val listReview = customerReviews.map {
+                "${it.review}\n- ${it.name}"
+            }
+            activityMainBinding.lvReview.adapter =
+                ArrayAdapter(this, R.layout.item_review, listReview)
+            activityMainBinding.edReview.setText("")
+        })
+
+        mainViewModel.isLoading.observe(this, { loading ->
+            activityMainBinding.progressBar.visibility =
+                if (loading) View.VISIBLE else View.GONE
+        })
+
+
     }
 }
---
+++ app/src/main/java/com/dicoding/ldwa/MainViewModel.kt
@@ -0,0 +1,61 @@
+package com.dicoding.ldwa
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.dicoding.ldwa.api.ApiConfig
+import com.dicoding.ldwa.model.CustomerReviewItems
+import com.dicoding.ldwa.model.Restaurant
+import com.dicoding.ldwa.model.RestaurantResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    private val _restaurant = MutableLiveData<Restaurant>()
+    val restaurant: LiveData<Restaurant> = _restaurant
+
+    private val _listReview = MutableLiveData<List<CustomerReviewItems>>()
+    val listReview: LiveData<List<CustomerReviewItems>> = _listReview
+
+    private val _isLoading = MutableLiveData<Boolean>()
+    val isLoading: LiveData<Boolean> = _isLoading
+
+    companion object {
+        private const val TAG = "MainViewModel"
+        private const val RESTAURANT_ID = "uewq1zg2zlskfw1e867"
+    }
+
+    init {
+        findRestaurant()
+    }
+
+    fun findRestaurant() {
+        _isLoading.value = true
+        val client = ApiConfig.getApiService().getRestaurant(RESTAURANT_ID)
+        client.enqueue(object : Callback<RestaurantResponse> {
+
+            override fun onResponse(
+                call: Call<RestaurantResponse>,
+                response: Response<RestaurantResponse>
+            ) {
+                _isLoading.value = false
+                if (response.isSuccessful) {
+                    _restaurant.value = response.body()?.restaurant
+                    _listReview.value = response.body()?.restaurant?.customerReviews
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<RestaurantResponse>, t: Throwable) {
+                _isLoading.value = false
+                Log.e(TAG, "onFailure: ${t.message.toString()}")
+            }
+
+        })
+    }
+
+}
